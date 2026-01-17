8bc5be8100274ad0499738b23a86f65fdd060f7a
ZHAO JIAYI <z261459808@gmail.com>
2026-01-17 18:11:53 +0900

    next

---
+++ nested-dynamic-routing/app/products/[id]/page.tsx
@@ -0,0 +1,5 @@
+// import { useState } from 'react';
+function page({}) {
+  return <>Prodcut Page</>;
+}
+export default page;
---
+++ nested-dynamic-routing/app/products/[id]/review/[reviewId]/page.tsx
@@ -0,0 +1,5 @@
+// import { useState } from 'react';
+function page() {
+  return <>Review Page</>;
+}
+export default page;
